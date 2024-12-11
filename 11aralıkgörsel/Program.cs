3d200479755dff4e1c1b56e9b49487406c1ede54
nazlisayar <verassyr81@gmail.com>
2024-12-11 16:03:35 +0300

    Proje dosyası ekle.

---
+++ 11aralıkgörsel/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11aralıkgörsel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            prizma P1= new prizma();   
+            prizma P2= new prizma();
+            prizma p3 = new prizma();   
+            prizma p4 = new prizma();
+            P1.seten(10);
+            P1.setboy(20);
+            P1.setyukseklik(30);
+            Console.WriteLine("sonuc:" + P1.gethacim());
+            P2.seten(5);
+            P2.setboy(10);
+            P2.setyukseklik(15);
+            Console.WriteLine("sonuc:"+P2.gethacim());
+            p3=P1+P2;   
+            Console.WriteLine("sonuc:"+p3.gethacim());
+            p4 = P1 - P2;
+            Console.WriteLine("sonuc:"+p4.gethacim());
+            if (P1 == P2)
+                Console.WriteLine("prizmalar eşittir");
+            else
+            {
+                Console.WriteLine("prizmalar eşit değildir.");
+            }
+
+            if(P1 == P2)
+                Console.WriteLine("prizmalar eşittir");
+            else
+            {
+                Console.WriteLine("prizmalar eşit değildir.");
+            }
+
+            if(P2==p4)
+                Console.WriteLine("prizmalar eşittir");
+            else
+            {
+                Console.WriteLine("prizmalar eşit değildir.");
+            }
+
+
+
+
+            Console.Read(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 11aralıkgörsel/prizma.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11aralıkgörsel
+{
+    internal class prizma
+    {
+        private double boy, en, yukseklik;
+        public void setboy(double a) { boy = a; }  
+        public void seten(double b) { en = b; }
+        public void setyukseklik (double c) { yukseklik = c; }
+        public double gethacim()
+        { return en*boy*yukseklik; }  
+        public static prizma operator +(prizma x,prizma y)
+        {
+            prizma z = new prizma();
+            z.en = x.en + y.en;
+            z.boy=x.boy+y.boy;  
+            z.yukseklik=x.yukseklik+y.yukseklik;
+            return z;   
+        }
+        public static prizma operator -(prizma x, prizma y) 
+        {
+            prizma z=new prizma();
+            z.en=x.en-y.en;
+            z.boy = x.boy - y.boy;
+            z.yukseklik=x.yukseklik-y.yukseklik;    
+            return z;   
+            
+
+
+        }
+        public static bool operator ==(prizma x,prizma y)
+        {
+            bool kosul;
+            if((x.en==y.en)&&(x.boy==y.boy)&&(x.yukseklik==y.yukseklik))
+            
+                kosul=true; 
+                else kosul=false;
+            return kosul;
+            
+        }
+        public static bool operator !=(prizma x, prizma y)
+        {
+            bool kosul = false;
+            if((x.en!=y.en)&&(x.boy!=y.boy)&&(x.yukseklik!=y.yukseklik))    
+                kosul=true;
+            else kosul=false;
+            return kosul;   
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
